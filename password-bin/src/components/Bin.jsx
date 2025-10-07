23517443cf0310d9e7fc98abb1cfca50514c0078
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2025-10-07 16:53:29 -0400

    changed bin flexbox to grid

--- password-bin/src/components/Bin.jsx
+++ password-bin/src/components/Bin.jsx
@@ -5,22 +5,23 @@ const Bin = () => {
         <>
             <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
 
-            <div className="container mx-auto bg-slate-600 max-w-4xl">
+            <div className="container mx-auto max-w-4xl">
 
-                <div className=" EntryZone flex flex-col p-4">
-                    <div>
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Save your Password</label>
-                        <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Site/App/Service" required />
-                    </div>
-                    <div className="credentials flex gap-3">
-                        <div>
-                            
-                            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="User Name" required />
-                            
-                            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
-                        </div>
+
+                <label for="name" class="block text-sm font-medium text-gray-900 dark:text-black mb-1">Manage your Passwords</label>
+                <div className="credentials">
+                    <div className='grid md:grid-cols-4 grid-cols-1 gap-y-4 gap-x-3'>
+
+                        <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-300 dark:border-gray-600 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 md:col-span-4" placeholder="Site/App/Service" required />
+
+                        <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-blue-300 dark:border-gray-600 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="User Name" required />
+
+                        <input type="password" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-blue-300 dark:border-gray-600 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Password" required />
+
+                        <input type="notes" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-blue-300 dark:border-gray-600 dark:placeholder-gray-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 md:col-span-2" placeholder="Notes (if any)" required />
                     </div>
                 </div>
+
             </div>
         </>
     )
--- password-bin/src/components/Navbar.jsx
+++ password-bin/src/components/Navbar.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
+import bin from '../assets/bin.svg'
 
 const Navbar = () => {
   return (
-    <nav className='bg-blue-300 flex justify-between p-3'>
-      <div className="logo">
-        PASSWORD BIN
+    <nav className='flex justify-between items-center p-5'>
+      <div className="logo duration-100 flex items-center">
+        <img src={bin} alt="Bin logo" className="h-22 w-22 mr-2" />
+        <span style={{ fontFamily: "'Bitcount Single Ink', sans-serif" }}
+          className='sm:text-2xl md:text-4xl'>PASSWORD BIN</span>
       </div>
-      <ul className='flex gap-4'>
-        <li>By Tarun Gattu</li>
-        <li>See more Work</li>
+      <ul className='flex md:gap-4 lg:gap-6 items-center'>
+        <li >
+          By{' '}
+          <a
+            href="https://www.linkedin.com/in/tarun-gattu123/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-700 md:text-2xl cursor-pointer"
+          >
+            Tarun Gattu
+          </a>
+        </li>
+
+
+        <li>
+          <a
+            href="https://tarungattu.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            See more Work
+          </a>
+        </li>
+
       </ul>
     </nav>
   )
